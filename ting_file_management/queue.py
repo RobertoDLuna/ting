9d1a43a9b6442ee9cfc537b70d096758835cad0f
Roberto Luna <robertocgw@gmail.com>
2023-03-07 14:52:18 -0300

    fix lint

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -5,15 +5,19 @@ class Queue(AbstractQueue):
     def __init__(self):
         # list() -> Estrutura para guardar infos da pilha
         self.list = list()
+
     # Retornar tamanho da lista
     def __len__(self):
         return len(self.list)
+
     # Adicionar dados
     def enqueue(self, value):
         return self.list.append(value)
+
     # Remover dados
     def dequeue(self):
         return self.list.pop(0)
+        
     # Buscar dados
     def search(self, index):
         # se o index for < 0 ou maior que o len da lista
