0980ceacef0ed8e9fe2a0de66d9efe46a190bf92
Roberto Luna <robertocgw@gmail.com>
2023-03-07 15:20:20 -0300

    feat: req2 - Implemente uma função txt_importer dentro do módulo file_management capaz de importar notícias a partir de um arquivo TXT, utilizando "\n" como separador.

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,18 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    # Caso o arquivo não tenha extensão txt
+    if not path_file.endswith('txt'):
+        # retorna erro de formato
+        return sys.stderr.write('Fomato inválido\n')
+    try:
+        # Abrindo o arquivo no modo leitura
+        with open(path_file, mode="r") as file:
+            # Abre o arquivo, retorna array splitado no \n
+            file_data = file.read().split('\n')
+            return file_data
+    # Se houver algo, lança exception FileNotFoundError
+    except FileNotFoundError:
+        # Retornar a mensagem final
+        return sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
